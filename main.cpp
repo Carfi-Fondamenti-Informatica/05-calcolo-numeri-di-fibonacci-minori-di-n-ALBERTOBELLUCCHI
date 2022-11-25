0ae2dcb69f6135e1698bfd97f9fd84821670d0ec
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-11-25 15:01:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n,num1=0,num2=1,nums, i;// nums>num2>num1
+    cin>>n;
+
+    cout << 1 << endl;
+    for(i=0;;i++){
+
+        nums = num1 + num2;//sommo
+        num1 = num2;//sposto
+        num2 = nums;
+
+        if(nums>n)break;
+        cout << nums << endl;
+
+    }
+
+    return 0;
 }
